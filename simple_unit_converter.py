3a30609742d8a214d77291d0d169711769777384
heyhenry <henryyy3@hotmail.com>
2025-01-01 10:25:24 +0800

    implemented simple function call checks

--- simple_unit_converter.py
+++ simple_unit_converter.py
@@ -9,14 +9,36 @@ Features:
 
 Stretch Goal: Add input validation to handle incorrect or non-numeric inputs.
 """
-class MainApp():
+class MainApp:
+    
     # selectable menu
-    def menu():
+    def menu(self):
         print("=== Menu ===")
         print("1. Kilometers to Miles")
         print("2. Celsius to Fahrenheit")
         print("3. Kilograms to Pounds")
         print("=== ==== ===")
+        
         user_choice = int(input("Select an Option: "))
 
-    
+        if user_choice == 1:
+            self.kilometers_to_miles()
+        elif user_choice == 2:
+            self.celsius_to_fahrenheit()
+        elif user_choice == 3:
+            self.kilograms_to_pounds()
+        else:
+            print("invalid user input!")
+            
+    def kilometers_to_miles(self):
+        print('kilometers!')
+
+    def celsius_to_fahrenheit(self):
+        print('celsius!')
+
+    def kilograms_to_pounds(self):
+        print('kg!')    
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.menu()
