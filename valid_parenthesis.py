a7caccc4018659dd85f5a5380e8cd2ebf0422e47
heyhenry <henryyy3@hotmail.com>
2025-01-28 10:52:52 +0800

    Added comments to valid parenthesis solution

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,13 +1,21 @@
 def valid_parenthesis(s : str) -> bool:
+    # initialize stack list to store parenthesis
     stack = []
-    pairs = {')': '(', ']': ']', '}': '{'}
+    # intialize pair dictionary for reference of parenthesis pairings
+    pairs = {')': '(', ']': '[', '}': '{'}
+    # iterate through the string of parenthesis
     for c in s:
+        # if its an opening parenthesis store in stack
         if c in '([{':
             stack.append(c)
+        # check if stack is empty
+        # compare last element in stack against pair's key value for match
         elif stack and stack[-1] == pairs.get(c):
             stack.pop()
+        # early exit if a closing parenthesis is to be added to the stack
         else:
             return False
+    # check if stack is empty for boolean result
     return not stack
 
 test_cases = [
