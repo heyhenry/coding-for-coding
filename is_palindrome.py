8338742cd4ea047483b7288d7bfafa49d752d6ae
heyhenry <henryyy3@hotmail.com>
2025-01-28 10:22:26 +0800

    Solved is palindrome leetcode problem (mathematics solution)

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,5 +1,18 @@
-def is_palindrome(x : int) -> bool:
-    return str(x) == str(x)[::-1]
+def is_palindrome(num : int) -> bool:
+    original_num = num
+    reverse_num = 0
+    while num > 0:
+        # find the last digit in num
+        tail = num % 10
+        # remove the last digit in num
+        num //= 10
+        # shift all digits in reverse_num 1 position to the left and add as a '0' placeholder
+        reverse_num *= 10
+        # add the tail digit to the end of the reverse_num
+        reverse_num += tail
+
+    # check if the original_num equals the reverse_num
+    return original_num == reverse_num
 
 test_cases = [
     121,
