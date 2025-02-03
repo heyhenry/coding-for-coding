2369363dab076e70a3bcf3d391e1da4f720ddc7c
heyhenry <henryyy3@hotmail.com>
2025-02-03 13:01:05 +0800

    Added in/pre/post order traversals to bst_II

--- trees/bst_II.py
+++ trees/bst_II.py
@@ -18,6 +18,28 @@ class TreeNode:
             else:
                 self.right.insert(val)
 
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(self.value)
+        if self.right:
+            self.right.inorder_traversal()
+
+    def preorder_traversal(self):
+        print(self.value)
+        if self.left:
+            self.left.preorder_traversal()
+        if self.right:
+            self.right.preorder_traversal()
+
+    def postorder_traversal(self):
+        if self.left:
+            self.left.postorder_traversal()
+        if self.right:
+            self.right.postorder_traversal()
+        print(self.value)
+
+
 tree = TreeNode(10)
 tree.insert(5)
 tree.insert(4)
@@ -27,3 +49,7 @@ tree.insert(3)
 tree.insert(22)
 tree.insert(11)
 tree.insert(12)
+
+# tree.inorder_traversal()
+# tree.preorder_traversal()
+tree.postorder_traversal()
