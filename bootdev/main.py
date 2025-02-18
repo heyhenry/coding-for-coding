3650b2ba3651718c5e8c82fee8f32f599c1199f5
heyhenry <henryyy3@hotmail.com>
2025-02-18 21:33:02 +0800

    Reduced code and increased efficiency of CH14L12 (Boot.Dev)

--- bootdev/main.py
+++ bootdev/main.py
@@ -3,21 +3,18 @@ class Unit:
         self.name = name
         self.pos_x = pos_x
         self.pos_y = pos_y
-
+    
     def in_area(self, x_1, y_1, x_2, y_2):
-        if (self.pos_x >= x_1 and self.pos_x <= x_2) and (self.pos_y >= y_1 and self.pos_y <= y_2):
-            return True
-        return False 
-
-
+        # direct boolean response in lieu of a if statement to determine 
+        return (self.pos_x >= x_1 and self.pos_x <= x_2) and (self.pos_y >= y_1 and self.pos_y <= y_2)
+    
 class Dragon(Unit):
     def __init__(self, name, pos_x, pos_y, fire_range):
         super().__init__(name, pos_x, pos_y)
         self.__fire_range = fire_range
 
     def breathe_fire(self, x, y, units):
-        hit_units = []
-        for u in units:
-            if u.in_area(x-self.__fire_range, y-self.__fire_range, x+self.__fire_range, y+self.__fire_range):
-                hit_units.append(u)
-        return hit_units
+        # used list comprehension in lieu of traditional for loop 
+        # reason: faster computation, noticeable for larger calculations due to less function calling (i.e. no need to call append())
+        return [unit for unit in units if unit.in_area(x-self.__fire_range, y-self.__fire_range, x+self.__fire_range, y+self.__fire_range)]
+        
