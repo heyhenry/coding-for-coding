54b9b56459eafe3b38181acab4d83e2631aefcec
heyhenry <henryyy3@hotmail.com>
2025-02-20 09:41:29 +0800

    Solved boot.dev assignment (CH5L10)

--- bootdev/main_test.py
+++ bootdev/main_test.py
@@ -1,38 +1,47 @@
 from main import *
 
 run_cases = [
-    (Dragon("Green Dragon", -1, -2, 1, 2, 1), -2, -3, 0, 0, True),
-    (Dragon("Red Dragon", 2, 2, 2, 2, 2), 0, 1, 1, 0, True),
-    (Dragon("Blue Dragon", 4, -3, 2, 1, 1), -5, -5, 5, 5, True),
-    (Dragon("Silver Dragon", 0, 0, 5, 5, 10), 4, 0, 5, 1, False),
-    (Dragon("Gold Dragon", 0, 0, 5, 5, 10), 4, 0, 5, 1, False),
-    (Dragon("Gold Dragon", 0, 0, 20, 20, 20), 10, 10, 20, 20, True),
+    (Sword("bronze"), Sword("bronze"), "iron", None),
+    (Sword("bronze"), Sword("iron"), None, "cannot craft"),
 ]
 
 submit_cases = run_cases + [
-    (Dragon("Green Dragon", -1, -2, 1, 2, 1), -3, -3, -1, -1, True),
-    (Dragon("Red Dragon", 2, 2, 2, 2, 2), 5, 5, 10, 10, False),
-    (Dragon("Blue Dragon", 4, -3, 2, 1, 1), 0, 0, 10, 10, False),
-    (Dragon("Black Dragon", 5, -1, 3, 2, 2), -10, -10, 10, 10, True),
-    (Dragon("White Dragon", 0, 0, 1, 1, 1), -1, -1, 1, 1, True),
+    (Sword("steel"), Sword("steel"), None, "cannot craft"),
+    (Sword("iron"), Sword("iron"), "steel", None),
+    (Sword("bronze"), Sword("steel"), None, "cannot craft"),
 ]
 
 
-def test(dragon, input1, input2, input3, input4, expected_output):
-    print("---------------------------------")
-    print(f"Inputs:")
-    print(
-        f" * Dragon position and size: {dragon.pos_x}, {dragon.pos_y}, {dragon.height}, {dragon.width}"
-    )
-    print(f" * Area corners: ({input1}, {input2}), ({input3}, {input4})")
-    print(f"Expected in area: {expected_output}")
-    result = dragon.in_area(input1, input2, input3, input4)
-    print(f"  Actual in area: {result}")
-    if result == expected_output:
-        print("Pass")
-        return True
-    print("Fail")
-    return False
+def test(sword1, sword2, expected_result, expected_err):
+    try:
+        print("---------------------------------")
+        print(
+            f"Crafting a {sword1.sword_type} sword with a {sword2.sword_type} sword..."
+        )
+        result = sword1 + sword2
+
+        if expected_err:
+            print(f"Expected Exception: {expected_err}")
+            print("Actual: no exception raised")
+            print("Fail")
+            return False
+
+        print(f"Expected: {expected_result}")
+        print(f"Actual: {result.sword_type}")
+        print(f"A new {result.sword_type} sword was crafted!")
+        if result.sword_type != expected_result:
+            print("Fail")
+            return False
+
+    except Exception as e:
+        print(f"Expected Exception: {expected_err}")
+        print(f"Actual Exception: {e}")
+        if expected_err != str(e):
+            print("Fail")
+            return False
+
+    print("Pass")
+    return True
 
 
 def main():
