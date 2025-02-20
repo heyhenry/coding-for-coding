b042ca9eda40735db26dbdf568e540f71e393984
heyhenry <henryyy3@hotmail.com>
2025-02-20 08:52:41 +0800

    Resolved is_palindrome leetcode.

--- leetcode/is_palindrome.py
+++ leetcode/is_palindrome.py
@@ -1,18 +1,18 @@
-def is_palindrome(num : int) -> bool:
-    original_num = num
-    reverse_num = 0
-    while num > 0:
-        # find the last digit in num
-        tail = num % 10
-        # remove the last digit in num
-        num //= 10
-        # shift all digits in reverse_num 1 position to the left and add as a '0' placeholder
-        reverse_num *= 10
-        # add the tail digit to the end of the reverse_num
-        reverse_num += tail
-
-    # check if the original_num equals the reverse_num
-    return original_num == reverse_num
+def is_palindrome(x : int) -> bool:
+    original_num = x
+    rev_num = 0
+    if x <= 0:
+        return False
+    while x != 0:
+        # get the tail digit
+        tail = x % 10
+        # push current digits in rev_num 1 position to the left
+        rev_num *= 10
+        # add the tail to the end of the rev_num
+        rev_num += tail
+        # remove the tail from the x
+        x //= 10
+    return original_num == rev_num
 
 test_cases = [
     121,
@@ -21,4 +21,4 @@ test_cases = [
 ]
 
 for i in range(len(test_cases)):
-    print(f"Test Case {i}: {is_palindrome(test_cases[i])}")
+    print(is_palindrome(test_cases[i]))
