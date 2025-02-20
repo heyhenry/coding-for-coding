e649ea6a872a0800019f74c92a34ca6336871900
heyhenry <henryyy3@hotmail.com>
2025-02-20 09:52:44 +0800

    Better solution for boot.dev assignment (CH5L10)

--- bootdev/main.py
+++ bootdev/main.py
@@ -3,9 +3,13 @@ class Sword:
         self.sword_type = sword_type
 
     def __add__(self, other):
-        if (self.sword_type == other.sword_type) and self.sword_type == 'bronze':
-            return Sword('iron')
-        elif (self.sword_type == other.sword_type) and self.sword_type == 'iron':
-            return Sword('steel')
-        else:
+        # dictionary indicating upgrade options of swords based on type
+        upgrade_options = {
+            'bronze': 'iron',
+            'iron': 'steel'
+        }
+        # validation check to see if swords match and/or is apart of the given selection range
+        if self.sword_type != other.sword_type or self.sword_type not in upgrade_options or other.sword_type not in upgrade_options:
             raise Exception("cannot craft")
+        # return a new sword if validation checks out (i.e sword matches were found)
+        return Sword(upgrade_options[self.sword_type])
