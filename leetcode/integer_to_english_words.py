e0307cdee0b459dab055455c70820512601ab018
heyhenry <henryyy3@hotmail.com>
2025-02-21 13:00:59 +0800

    Updated solution (but still have bugs to fix)

--- leetcode/integer_to_english_words.py
+++ leetcode/integer_to_english_words.py
@@ -1,38 +1,120 @@
 class Solution:
     def number_to_word(self, num : int) -> str:
-        # constraints: 0 <= num <= 2147483648 - 1
-        # 2,147,483,648
+        if num == 0:
+            return 'Zero'
+        
         num = str(num)
+        
+        # base digit conversion to english 
+        base_num = {
+            '1': 'One',
+            '2': 'Two',
+            '3': 'Three',
+            '4': 'Four',
+            '5': 'Five',
+            '6': 'Six',
+            '7': 'Seven',
+            '8': 'Eight',
+            '9': 'Nine'
+        }
+
+        ten_base = {
+            '1': 'teen',
+            '2': 'Twenty',
+            '3': 'Thirty',
+            '4': 'Forty',
+            '5': 'Fifty',
+            '6': 'Sixty',
+            '7': 'Seventy',
+            '8': 'Eighty',
+            '9': 'Ninety'
+        }
+
         num_length = len(num)
+        check_num_length = 0
         counter = 0
-        lst = []
         section = ''
-        total_num_length = 0
-        # splitting the numbers into sections of 3s
-        for i in num:
-            section += i
+        lst = []
+        group_counter = 0
+
+        # group digits into 3s from left to right
+        for i in num[::-1]:
             counter += 1
-            # resetting the counter, adding the section to the lst list, resetting the counter and section variables
+            section += i
             if counter == 3:
-                lst.append(section)
+                group_counter += 1
+                # reverse each group of numbers to correlate with base num
+                lst.append((section[::-1], group_counter))
                 counter = 0
                 section = ''
-        # checking the length of the sum length of the elements in the lst list
+
+        # supply num length checker variable with existing list element length
         for i in lst:
-            total_num_length += len(i)
-        # determines whether there are digits unaccounted for in the lst list
-        if total_num_length < num_length:
-            lst.append(section)
+            check_num_length += len(i[0])
+
+        # check if lst has all elements from num
+        if check_num_length < num_length:
+            lst.append((section[::-1], group_counter+1))
+
+        # reverse the list to reflect groupings of 3s to reflect base num's order
+        lst = lst[::-1]
+
+        positional_values = {
+            2: 'Thousand',
+            3: 'Million',
+            4: 'Billion'
+        }
+        special_nums = {
+            '10': 'Ten',
+            '11': 'Eleven',
+            '12': 'Twelve',
+            '13': 'Thirteen',
+            '14': 'Fourteen',
+            '15': 'Fifteen',
+            '16': 'Sixteen',
+            '17': 'Seventeen',
+            '18': 'Eighteen',
+            '19': 'Nineteen'
+        }
+
+        full_word = ''
+        for i in lst:
+            the_word = ''
+            if len(i[0]) == 3:
+                the_word += (f"{base_num[i[0][0]]} " + 'Hundred ') 
+                if int(i[0][1]+i[0][2]) in range(10, 20):
+                    the_word += f"{special_nums[i[0][1]+i[0][2]]}"
+                else:
+                    if i[0][2] == '0':
+                        the_word += f"{ten_base[i[0][1]]} "
+                    else:
+                        the_word += f"{ten_base[i[0][1]]} "
+                        the_word += f"{base_num[i[0][2]]} "
+            elif len(i[0]) == 2:
+                if int(i[0]) in range(10,20):
+                    the_word += f"{special_nums[[i][0][0]]} " 
+                else:
+                    if i[0][1] == '0':
+                        the_word += f"{ten_base[i[0][0]]} "
+                    else:
+                        the_word += f"{ten_base[i[0][0]]} "
+                        the_word += f"{base_num[i[0][1]]} "
+            else:
+                the_word += f"{base_num[i[0]]} "
+            if i[1] in positional_values:
+                the_word += f"{positional_values[i[1]]} "
+            full_word += the_word
+        
+        full_word = full_word.strip()
+
+        return full_word
     
-        
-        
-
-
-
-
 solution = Solution()
 
-print(solution.number_to_word(123))
+print(solution.number_to_word(12003))
+
+# Current Issue: 
+# cant deal with more than 1 0 on the end i.e. 100, 1000, 100000, 1000000 also stuff like 12003
 
 # test_cases = [
 #     123,
