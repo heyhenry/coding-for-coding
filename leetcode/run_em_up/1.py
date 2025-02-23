e90766554d988a81776bda2df57d4b0e6ce7ba71
heyhenry <henryyy3@hotmail.com>
2025-02-23 12:14:28 +0800

    Re-solved leetcode 1. Two Sum

--- leetcode/run_em_up/1.py
+++ leetcode/run_em_up/1.py
@@ -1,45 +1,23 @@
-# Credit to leetcode user: Rahul Varma for the 2nd and 3rd solution insights
-
 class Solution:
     def two_sum(self, nums : list[int], target : int) -> list[int]:
-        # nested for loop solution (aka brute force)
-        # size = len(nums)
-        # for i in range(size):
-        #     for j in range(i+1, size):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        # return []
-
-        # double loop solution 
-        # nums_dict = {}
-        # size = len(nums)
-        # for i in range(size):
-        #     nums_dict[nums[i]] = i
-
-        # for i in range(size):
-        #     complement = target - nums[i]
-        #     if complement in nums_dict and nums_dict[complement] != i:
-        #         return [i, nums_dict[complement]]
-
-        # single loop solution
-        size = len(nums)
+        # single loop method
         nums_dict = {}
+        size = len(nums)
+
         for i in range(size):
-            compl = target - nums[i]
-            if compl in nums_dict:
-                return [nums_dict[compl], i]
+            complement = target - nums[i]
+            if complement in nums_dict:
+                return [nums_dict[complement], i]
             nums_dict[nums[i]] = i
         return []
 
 solution = Solution()
 
-print(solution.two_sum([2,7,11,15], 9))
+test_cases = [
+    ([2,7,11,15], 9),
+    ([3,2,4], 6),
+    ([3,3], 6)
+]
 
-# test_cases = [
-#     ([2,7,11,15], 9),
-#     ([3,2,4], 6),
-#     ([3,3], 6)
-# ]
-
-# for i in range(len(test_cases)):
-#     print(solution.two_sum(test_cases[i][0], test_cases[i][1]))
+for i in range(len(test_cases)):
+    print(solution.two_sum(test_cases[i][0], test_cases[i][1]))
