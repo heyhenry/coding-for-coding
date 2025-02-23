3fa68163b047d231e51a7b799617de5e8a10ac1c
heyhenry <henryyy3@hotmail.com>
2025-02-24 07:57:29 +0800

    Some mental note to grasp logic for leetcode 13 alternative solution.

--- leetcode/run_em_up/13.py
+++ leetcode/run_em_up/13.py
@@ -47,8 +47,14 @@ class Solution:
             else:
                 result += roman_map[s[i]]
         return result
-                        
-
+        # mental issue grasping the solution in its entirey, understood the gist but 
+        # couldnt completely wrap head around it in full mental clarity -
+        # after some time found a good way to look at it that helped wrap it up in my head
+        # the way: the reason for the -= is because it knows the next one is gonna be a big one i.e. 
+        # current one is I, next one is V so if it's IV its 4 instead of I+V = 6
+        # so the current i = I will start by negating the value before hand as the rest of the value will be supplemented by its upcoming duo/partner the V
+        # i.e. result = 0 -> result -= I (1) and then result = -1 -> result += I (5) which will give a result of 4 which is reflective of IV
+                    
 
 solution = Solution()
 
