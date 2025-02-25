fe32d5ddbf781caf97c41b015f649fd625507707
heyhenry <henryyy3@hotmail.com>
2025-02-25 09:41:42 +0800

    Alternative solution for 20. Valid Parenthesis

--- leetcode/run_em_up/20.py
+++ leetcode/run_em_up/20.py
@@ -1,12 +1,26 @@
 class Solution:
     def is_valid(self, s : str) -> bool:
-        # method 1
-        parenthesis = {')': '(', ']': '[', '}': '{'}
+        # method 1 (beats 100% of peers)
+        # parenthesis = {')': '(', ']': '[', '}': '{'}
+        # stack = []
+        # for p in s:
+        #     if p in '([{':
+        #         stack.append(p)
+        #     elif len(stack) > 0 and p in parenthesis and parenthesis[p] == stack[-1]:
+        #         stack.pop()
+        #     else:
+        #         return False
+        # return len(stack) == 0
+
+        # method 2 (also beats 100% of peers)
         stack = []
-        for p in s:
-            if p in '([{':
-                stack.append(p)
-            elif len(stack) > 0 and p in parenthesis and parenthesis[p] == stack[-1]:
+        size = len(s)
+        for i in range(size):
+            if s[i] in '([{':
+                stack.append(s[i])
+            elif stack and (s[i] == ')' and stack[-1] == '(' or 
+                            s[i] == ']' and stack[-1] == '[' or 
+                            s[i] == '}' and stack[-1] == '{'):
                 stack.pop()
             else:
                 return False
@@ -18,7 +32,8 @@ test_cases = [
     "()",
     "()[]{}",
     "(]",
-    "([])"
+    "([])",
+    ']'
 ]
 
 for i in range(len(test_cases)):
