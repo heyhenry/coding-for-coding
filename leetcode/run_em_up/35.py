c5abc09f89c91f9a214f6b577ee95868bbc54d49
heyhenry <henryyy3@hotmail.com>
2025-02-26 06:54:38 +0800

    Added code logic comment to leetcode 35

--- leetcode/run_em_up/35.py
+++ leetcode/run_em_up/35.py
@@ -1,14 +1,19 @@
 class Solution:
     def search_insert(self, nums : list[int], target : int) -> int:
+        # list size variable <-- less len() executions
         size = len(nums)
+        # early return if target value is lower than initial element in the list
         if target < nums[0]:
             return 0
+        # algorithm to check if a match is found, else determine possible insertion position
         for i in range(size):
+            # found match
             if nums[i] == target:
                 return i
+            # found insertable position in list
             elif i > 0 and target < nums[i] and target > nums[i-1]:
                 return i
-
+        # target value is found to be the largest value
         return size
         
 solution = Solution()
