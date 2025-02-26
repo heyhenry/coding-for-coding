c0bd847a95d1979e9274f438d930262f44383910
heyhenry <henryyy3@hotmail.com>
2025-02-26 08:07:25 +0800

    Added code logic comments to leetcode solution 58

--- leetcode/run_em_up/58.py
+++ leetcode/run_em_up/58.py
@@ -2,15 +2,24 @@ class Solution:
     def length_of_last_word(self, s : str) -> int:
         words = []
         word = ''
+        # loop through each character in the 's' string
         for c in s:
+            # check is a character is a letter
             if c.isalpha():
+                # append to word
                 word += c
             else:
+                # checker incase of empty word value
                 if len(word) > 0:
+                    # if not empty word value, add word to the words list
                     words.append(word)
+                    # reset word value
                     word = ''
+        # check if the word value is a valid string
         if len(word) > 0:
+            # if so, add word to the words list
             words.append(word)
+        # return the length of the last element in the words list
         return len(words[-1])
 
 solution = Solution()
