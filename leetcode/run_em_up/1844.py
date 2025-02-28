a9232d5c951ff64164d0bfb8e4d340f7bfed659a
heyhenry <henryyy3@hotmail.com>
2025-03-01 05:57:21 +0800

    Alternative solution for leetcode 1844. Reduced Code

--- leetcode/run_em_up/1844.py
+++ leetcode/run_em_up/1844.py
@@ -1,24 +1,35 @@
 class Solution:
     def replace_digits(self, s : str) -> str:
+        # solution 1
         # shift func
-        def shift(c : str, i : int):
-           # finds the new character based on unicode maths
-            return chr(ord(c)+int(i))
-        size = len(s)
+        # def shift(c : str, i : int):
+        #    # finds the new character based on unicode maths
+        #     return chr(ord(c)+int(i))
+        # size = len(s)
+        # new_s = ''
+        # # iterates through the s string and creates a new updated string s
+        # # replacing digits with leters
+        # for i in range(size):
+        #     # if the element (string) at this position in the string is a digit
+        #     if s[i].isdigit():
+        #         # then invoke the shift method and convert to the appropriate letter
+        #         new_s += shift(s[i-1], s[i])
+        #     else:
+        #         # if not, then add normal letter to new string s
+        #         new_s += s[i]
+        # # update all element (letters etc) in the old s string with those of the new s string
+        # s = new_s[:]
+        # # return updated string s
+        # return s
+
+        # solution 2 - reduced code based on solution 1
         new_s = ''
-        # iterates through the s string and creates a new updated string s
-        # replacing digits with leters
-        for i in range(size):
-            # if the element (string) at this position in the string is a digit
+        for i in range(len(s)):
             if s[i].isdigit():
-                # then invoke the shift method and convert to the appropriate letter
-                new_s += shift(s[i-1], s[i])
+                new_s += chr(ord(s[i-1])+int(s[i]))
             else:
-                # if not, then add normal letter to new string s
                 new_s += s[i]
-        # update all element (letters etc) in the old s string with those of the new s string
         s = new_s[:]
-        # return updated string s
         return s
 
 solution = Solution()
