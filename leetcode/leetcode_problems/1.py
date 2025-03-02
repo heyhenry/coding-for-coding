5a3e3ca4b7325e2261ef0f4ec61b7625f2bdaf2e
heyhenry <henryyy3@hotmail.com>
2025-03-02 12:52:15 +0800

    Solved leetcode no.1

---
+++ leetcode/leetcode_problems/1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def two_sum(self, nums : list[int], target : int) -> list[int]:
+        size = len(nums)
+        for i in range(size):
+            for j in range(i+1, size):
+                if nums[i]+nums[j] == target:
+                    return [i, j]
+    
+solution = Solution()
+test_cases = [
+    ([2,7,11,15], 9), 
+    ([3,2,4], 6),
+    ([3,3], 6)
+]
+for i in range(len(test_cases)):
+    print(solution.two_sum(test_cases[i][0], test_cases[i][1]))
