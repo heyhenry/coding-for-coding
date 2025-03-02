95d0847e7e714c95cd251cfffd6fd21b394d030a
heyhenry <henryyy3@hotmail.com>
2025-03-03 04:15:48 +0800

    Added 2 alternative solution to leetcode 1. Two Sum

--- leetcode/leetcode_problems/1.py
+++ leetcode/leetcode_problems/1.py
@@ -1,11 +1,33 @@
 class Solution:
     def two_sum(self, nums : list[int], target : int) -> list[int]:
+        # solution 1 (brute force)
+        # size = len(nums)
+        # for i in range(size):
+        #     for j in range(i+1, size):
+        #         if nums[i]+nums[j] == target:
+        #             return [i, j]
+    
+        # 2 loops solution
+        # nums_map = {}
+        # size = len(nums)
+        # for i in range(size):
+        #     nums_map[nums[i]] = i
+        # for i in range(size):
+        #     part = target - nums[i]
+        #     if part in nums_map and nums_map[part] != i:
+        #         return [i, nums_map[part]] # <--- the indexes can be in any order, because the dictionary was created before this loop, so part can be indexed anywhere
+        # return []
+
+        # 1 loop solution
+        nums_map = {}
         size = len(nums)
         for i in range(size):
-            for j in range(i+1, size):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
-    
+            component = target - nums[i]
+            if component in nums_map:
+                return [nums_map[component], i]
+            nums_map[nums[i]] = i
+        return []
+
 solution = Solution()
 test_cases = [
     ([2,7,11,15], 9), 
