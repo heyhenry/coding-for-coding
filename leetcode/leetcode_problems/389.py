5dc31f2f4c05b0d3a1c59da24a8fadc3c037efc4
heyhenry <henryyy3@hotmail.com>
2025-03-03 14:56:52 +0800

    Alternative solution for leetcode 389. Find the Difference

--- leetcode/leetcode_problems/389.py
+++ leetcode/leetcode_problems/389.py
@@ -1,19 +1,27 @@
 class Solution:
     def find_the_difference(self, s : str, t : str) -> str:
-        t_dict = {}
-        for i in t:
-            if i not in t_dict:
-                t_dict[i] = 1
-            else:
-                t_dict[i] += 1
+        # my solution
+        # t_dict = {}
+        # for i in t:
+        #     if i not in t_dict:
+        #         t_dict[i] = 1
+        #     else:
+        #         t_dict[i] += 1
         
-        for i in s:
-            if i in t_dict and t_dict[i] != 0:
-                t_dict[i] -= 1
+        # for i in s:
+        #     if i in t_dict and t_dict[i] != 0:
+        #         t_dict[i] -= 1
         
-        for key, val in t_dict.items():
-            if val == 1:
-                return key
+        # for key, val in t_dict.items():
+        #     if val == 1:
+        #         return key
+
+        # glimpsed ascii solution i tried without fully looking
+        # i dont need to add the square bracketse to turn this into a list comprehension cos sum() takes in generator expressions aka the below.
+        # essentially opt for the below way (i.e. exclude the square brakets) because including the square brackets means itll create the whole list before doing sum. aka an extra step.
+        s1 = sum(ord(c) for c in s)
+        t1 = sum(ord(c) for c in t)
+        return chr(t1-s1)
     
 solution = Solution()
 test_cases = [
