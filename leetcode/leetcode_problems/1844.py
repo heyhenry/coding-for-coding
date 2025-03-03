a05b66f19e140b263e0d8c3352fe9b97ac0eddcf
heyhenry <henryyy3@hotmail.com>
2025-03-03 20:07:01 +0800

    Alternative solution for leetcode 1844

--- leetcode/leetcode_problems/1844.py
+++ leetcode/leetcode_problems/1844.py
@@ -3,14 +3,23 @@ class Solution:
         return chr(ord(c)+int(i))
     
     def replace_digits(self, s : str) -> str:
+        # method 1
+        # size = len(s)
+        # new_s = ''
+        # for c in range(size):
+        #     if s[c].isdigit():
+        #         new_s += self.shift(s[c-1], int(s[c]))
+        #     else:
+        #         new_s += s[c]
+        # return new_s
+
+        # method 2
+        s = [c for c in s]
         size = len(s)
-        new_s = ''
-        for c in range(size):
-            if s[c].isdigit():
-                new_s += self.shift(s[c-1], int(s[c]))
-            else:
-                new_s += s[c]
-        return new_s
+        for i in range(size):
+            if s[i].isdigit():
+                s[i] = self.shift(s[i-1], int(s[i]))
+        return "".join(s)
 
 solution = Solution()
 test_cases = [
