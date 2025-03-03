8eb556b2dfa7380a6f2c697268732469e58e01f2
heyhenry <henryyy3@hotmail.com>
2025-03-03 09:07:22 +0800

    Alternative solution to leetcode 9. Palindrome Number

--- leetcode/leetcode_problems/9.py
+++ leetcode/leetcode_problems/9.py
@@ -1,7 +1,22 @@
 class Solution:
     def is_palindrome(self, x :int) -> bool:
         # string solution
-        return str(x) == str(x)[::-1]
+        # return str(x) == str(x)[::-1]
+
+        # mathematical solution
+        rev_num = 0
+        og = x
+
+        if x < 0:
+            return False
+
+        while x != 0:
+            tail = x % 10
+            rev_num *= 10
+            rev_num += tail
+            x //= 10
+        
+        return og == rev_num
 
 solution = Solution()
 test_cases = [
