b436141ddd4111fd0fff317f1775c4f6ae70417b
heyhenry <henryyy3@hotmail.com>
2025-03-03 13:11:19 +0800

    Alternative solution for 557. Reverse Words in a String III

--- leetcode/leetcode_problems/557.py
+++ leetcode/leetcode_problems/557.py
@@ -1,14 +1,21 @@
 class Solution:
     def reverse_words(self, s : str) -> str:
+        # first solution
+        # s = s.split()
+        # size = len(s)
+        # new_s = ''
+        # for i in range(size):
+        #     if i != size-1:
+        #         new_s += (s[i][::-1] + ' ')
+        #     else:
+        #         new_s += s[i][::-1]
+        # return new_s
+
+        # 2nd solution using .join() 
         s = s.split()
-        size = len(s)
-        new_s = ''
-        for i in range(size):
-            if i != size-1:
-                new_s += (s[i][::-1] + ' ')
-            else:
-                new_s += s[i][::-1]
-        return new_s
+        for i in range(len(s)):
+            s[i] = s[i][::-1]
+        return ' '.join(s)
 
 solution = Solution()
 test_cases = [
