cd7576ce7cb1637564f42a4e6db053a21e3af8b2
heyhenry <henryyy3@hotmail.com>
2025-03-04 10:46:59 +0800

    Alternative leetcode solution 268. Missing Number

--- leetcode/leetcode_problems/268.py
+++ leetcode/leetcode_problems/268.py
@@ -1,9 +1,18 @@
 class Solution:
     def missing_number(self, nums : list[int]) -> int:
+        # method 1
+        # n = len(nums)
+        # for i in range(n+1):
+        #     if i not in nums:
+        #         return i
+
+        # method 2
+        nums = sorted(nums)
         n = len(nums)
-        for i in range(n+1):
-            if i not in nums:
+        for i in range(n):
+            if i != nums[i]:
                 return i
+        return n
 
 solution = Solution()
 test_cases = [
