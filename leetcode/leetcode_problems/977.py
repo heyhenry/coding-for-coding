03d88b856208804d1c38aa1d64baacc116af7007
heyhenry <henryyy3@hotmail.com>
2025-03-04 12:47:32 +0800

    Alternative leetcode solution (2 pointer) 977

--- leetcode/leetcode_problems/977.py
+++ leetcode/leetcode_problems/977.py
@@ -9,6 +9,24 @@ class Solution:
         #     nums[i] = nums[i]**2
         # return sorted(nums)
 
+        # after seeing this solution, understood it and did it without looking at reference again
+        # 2 pointer solution
+        size = len(nums)
+        left, right = 0, size-1
+        result = [0]*size
+        for i in range(size-1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                value = nums[left]
+                left += 1
+            else:
+                value = nums[right]
+                right -= 1
+            result[i] = value ** 2
+        return result
+            
+
+
+
 solution = Solution()
 test_cases = [
     [-4,-1,0,3,10],
