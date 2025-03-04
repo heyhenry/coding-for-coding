2048e345d2bbd9dd3f88a5e4a389b8e8d10a8f1d
heyhenry <henryyy3@hotmail.com>
2025-03-04 08:12:53 +0800

    Alternative leetcode solution 2418. Sort the People

--- leetcode/leetcode_problems/2418.py
+++ leetcode/leetcode_problems/2418.py
@@ -1,13 +1,25 @@
 class Solution:
     def sort_people(self, names : list[str], heights : list[int]) -> list[str]:
+        # method 1
+        # size = len(names)
+        # hn_dict = {}
+        # updated_names = []
+        # for i in range(size):
+        #     hn_dict[heights[i]] = names[i]
+        # for key, val in sorted(hn_dict.items(), reverse=True):
+        #     updated_names.append(val)
+        # return updated_names
+
+        # method 2
         size = len(names)
-        hn_dict = {}
-        updated_names = []
+        d = {}
         for i in range(size):
-            hn_dict[heights[i]] = names[i]
-        for key, val in sorted(hn_dict.items(), reverse=True):
-            updated_names.append(val)
-        return updated_names
+            d[heights[i]] = names[i]
+        counter = 0
+        for key, val in sorted(d.items(), reverse=True):
+            names[counter] = val
+            counter += 1
+        return names
 
 solution = Solution()
 test_cases = [
