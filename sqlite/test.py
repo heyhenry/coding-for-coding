1604bee665dc6506fbf1db02341bbf31dd798157
heyhenry <henryyy3@hotmail.com>
2025-03-10 16:50:08 +0800

    Create first interaction with sqlite (incl. creating db, inserting data to db)

---
+++ sqlite/test.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+# create the 'test.db' file if it does not exist, else connect to the existing 'test.db'
+connection = sqlite3.connect(r"test.db")
+# create cursor handle for invoking methods like executing SQLite statements and fetching data
+cursor = connection.cursor()
+
+# create the accounts table if it does not exist
+create_table_statement = '''
+CREATE TABLE IF NOT EXISTS accounts (
+    Account_Name TEXT,
+    Username TEXT,
+    Password TEXT
+);
+'''
+
+# execute the statement to create a table if it does not exist
+cursor.execute(create_table_statement)
+
+# insert values for a new account
+insert_account_statement = " INSERT INTO accounts (Account_Name, Username, Password) values ('Crunchyroll', 'Henryyy', 'Kokoro91') "
+cursor.execute(insert_account_statement)
+
+# save changes to the database
+connection.commit()
+
+# close the database
+connection.close()
