248abd8e6a53e7d4928a13a89def6ca68d5e342c
heyhenry <henryyy3@hotmail.com>
2025-03-14 02:03:59 +0800

    Updated the main.py with manual check option and amended functions

--- sqlite/encryption/main.py
+++ sqlite/encryption/main.py
@@ -1,10 +1,11 @@
 import sqlite3
 from cryptography.fernet import Fernet
 import os
-import shutil
 
-connection = sqlite3.connect("sample_database.db")
-cursor = connection.cursor()
+if os.path.exists("sample_database.db"):
+    connection = sqlite3.connect("sample_database.db")
+    cursor = connection.cursor()
+
 key = Fernet("UBy1tA_tENd8h7NfXqNwH5c5Sx2H2CkqXbN1RXJYkFY=")
 
 # create a dump.sql script of the given database
@@ -19,16 +20,21 @@ def create_dump():
 def recreate_database():
     with open("dump.sql", 'r') as file:
         sql_script = file.read()
+    delete_dump('dump.sql')
+    connection = sqlite3.connect("sample_database.db")
     connection.executescript(sql_script)
     connection.commit()
+    connection.close()
+    quit()
 
 # delete the dump.sql
 def delete_dump(filename):
     if os.path.exists(filename):
-        os.remove("dump.sql")
+        os.remove(filename)
 
 # delete the database.db
 def delete_database():
+    connection.close()
     if os.path.exists("sample_database.db"):
         os.remove("sample_database.db")
 
@@ -50,7 +56,22 @@ def decrypt_dump():
         with open('dump.sql', 'wb') as outfile:
             outfile.write(decrypted_data)
     delete_dump('encrypted_dump.sql')
-    
 
-connection.close()
+print('menu')
+print("1. Create Dump")
+print("2. Encrypt Dump")
+print("3. Decrypt Dump")
+print("4. Recreate Database")
+choice = int(input("Pick: "))
 
+if choice == 1:
+    create_dump()
+elif choice == 2:
+    encrypt_dump()
+elif choice == 3:
+    decrypt_dump()
+elif choice == 4:
+    recreate_database()
+
+if os.path.exists('sample_database.db'):
+    connection.close()
