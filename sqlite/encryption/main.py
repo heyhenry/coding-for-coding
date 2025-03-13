d5b1fbc8211d218518e5e797365533ff684a3a74
heyhenry <henryyy3@hotmail.com>
2025-03-14 01:34:41 +0800

    Updated all functions in the sqlite/encryption/main.py

--- sqlite/encryption/main.py
+++ sqlite/encryption/main.py
@@ -7,46 +7,50 @@ connection = sqlite3.connect("sample_database.db")
 cursor = connection.cursor()
 key = Fernet("UBy1tA_tENd8h7NfXqNwH5c5Sx2H2CkqXbN1RXJYkFY=")
 
+# create a dump.sql script of the given database
 def create_dump():
     with open("dump.sql", 'w') as outfile:
         for line in connection.iterdump():
             outfile.write(f"{line}\n")
+    # remove the database, we have the sql script
+    delete_database()
 
-def restore_dump():
+# recreate the database based on the dump.sql script
+def recreate_database():
     with open("dump.sql", 'r') as file:
         sql_script = file.read()
     connection.executescript(sql_script)
     connection.commit()
 
-def delete_dump():
-    if os.path.exists("dump.sql"):
+# delete the dump.sql
+def delete_dump(filename):
+    if os.path.exists(filename):
         os.remove("dump.sql")
 
+# delete the database.db
 def delete_database():
     if os.path.exists("sample_database.db"):
         os.remove("sample_database.db")
 
+# create an encrypted version of the dump.sql script
 def encrypt_dump():
     with open('dump.sql', 'rb') as file:
         data = file.read()
         encrypted_data = key.encrypt(data)
         with open('encrypted_dump.sql', 'wb') as outfile:
             outfile.write(encrypted_data)
+    # delete the unencrypted dump.sql script
+    delete_dump('dump.sql')
 
+# create a decrypted version of the dump.sql script
 def decrypt_dump():
     with open('encrypted_dump.sql', 'rb') as file:
         data = file.read()
         decrypted_data = key.decrypt(data)
         with open('dump.sql', 'wb') as outfile:
             outfile.write(decrypted_data)
+    delete_dump('encrypted_dump.sql')
     
-
-# create_dump()
-# restore_dump()
-# delete_dump()
-# delete_database()
-# encrypt_dump()
-decrypt_dump()
 
 connection.close()
 
